5277637cc7ce1bf9411563d773953b872d40d4a2
Jonathan <jonitoadriano@gmail.com>
2022-12-06 14:25:09 -0600

    create data in seed db and tested with Product.all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#Create a loop to generate fake data
+20.times do
+testproduct = Product.create(name: "tsst ", price: "1223 ", description: " prot eudher dme", image_url: " feifj eirj")
+end
