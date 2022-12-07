7826ad3fe40a9551844748e5ee2a73f408c3b8ac
Jonathan <jonitoadriano@gmail.com>
2022-12-07 13:11:56 -0600

    create post RESTful code to add new product to list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,15 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product.as_json
   end
-  
+
+  def create
+    product1 = Product.new(
+      name: "Knife",
+      price: 10,
+      image_url: "google",
+      description: "A sharp metallic blade"
+    )
+    product1.save
+    render json: product1.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"  
 
   get "/products" => "products#index"
+
+  post "/products" => "products#create"
 end
