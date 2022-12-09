15af121d38f14fa53ef88cccd283f0ba879cb15a
Jonathan <jonitoadriano@gmail.com>
2022-12-09 13:48:08 -0600

    created new database rows and updated old data type

---
+++ db/migrate/20221209194052_add_stock_to_products.rb
@@ -0,0 +1,8 @@
+class AddStockToProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :stock, :integer
+    change_column :products, :description, :text
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+  end
+end
