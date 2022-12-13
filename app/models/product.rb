9dfda0c17bde869fcfdb9b711c185b7351003693
Jonathan <jonitoadriano@gmail.com>
2022-12-13 13:09:04 -0600

    add supplier and product link

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
-  # validates :name, presence: true
-  # validates :name, uniqueness: true
-  # validates :price, presence: true
-  # validates :description, presence: true
-  # validates :description, length:{in: 10..500}
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :description, length:{in: 10..500}
+  belongs_to: suppliers
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
 end
