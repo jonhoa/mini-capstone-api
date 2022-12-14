c7c3e4710cb3c0b403af6e3611094af3c24fc54f
Jonathan <jonitoadriano@gmail.com>
2022-12-14 12:59:22 -0600

    add user and password hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation]
     )
     if user.save
-      render json: {message: "User created succesfully"}, status: :created
+      render json: { message: "User created successfully" }, status: :created
     else
-      render json: {errors: user.errors.full_messages}, status: :bad_request
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 end
