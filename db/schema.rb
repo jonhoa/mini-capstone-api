9357a697dc681c3dc3f23acf100d3a8253e96ad9
Jonathan <jonitoadriano@gmail.com>
2022-12-15 14:32:14 -0600

    add admin boolean column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_173443) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_203058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_173443) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
