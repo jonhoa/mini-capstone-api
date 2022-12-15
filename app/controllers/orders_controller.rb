916e8fe91781e9a89f5fcdc3cc4626808c0d5425
Jonathan <jonitoadriano@gmail.com>
2022-12-15 11:52:41 -0600

    create show REST for orders model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,4 +11,9 @@ class OrdersController < ApplicationController
     order.save
     render json: order.as_json
   end
+
+  def show
+    order = Order.find_by(id: params[:id])
+    render json: order.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   post "/orders" => "orders#create"
+
+  get "/orders/:id" => "orders#show"
 end
